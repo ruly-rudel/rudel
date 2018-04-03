123c3cd845be60393d8855ebdbe311de3e4bc8cb
Ruly Rudel <ruly.official@gmail.com>
2018-04-03 17:51:02 +0900

    remove unused files. typically eval.

--- src/core.h
+++
@@ -1,9 +0,0 @@
-#ifndef _core_h_
-#define _core_h_
-
-#include "misc.h"
-#include "builtin.h"
-
-value_t	create_root_env	(void);
-
-#endif // _core_h_
--- src/resolv.h
+++
@@ -1,8 +0,0 @@
-#ifndef _resolv_h_
-#define _resolv_h_
-
-#include "builtin.h"
-
-value_t resolv_bind	(value_t v, value_t env);
-
-#endif  // _resolv_h_
